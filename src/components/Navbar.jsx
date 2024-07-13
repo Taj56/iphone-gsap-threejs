80ed12213ce4a0568a82c17e9bd05b9dfd744aa8
Taj56 <64923628+Taj56@users.noreply.github.com>
2024-07-13 14:35:10 -0500

    highlights heading

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,16 @@ import { appleImg, bagImg, searchImg } from '../utils';
 const Navbar = () => {
   return (
     <header className="flex w-full items-center justify-between p-5 sm:px-10">
-      <nav className="flex w-full screen-max-width">
+      <nav className="screen-max-width flex w-full">
         <img src={appleImg} alt="apple logo" width={14} height={18} />
 
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav, i) => (
-            <div key={i} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>{nav}</div>
+            <div key={i} className='cursor-pointer px-5 text-sm text-gray transition-all hover:text-white'>{nav}</div>
           ))}
         </div>
 
-        <div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
+        <div className='flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end'>
           <img src={searchImg} alt="search" width={18} height={18} />
           <img src={bagImg} alt="bag" width={18} height={18} />
         </div>
